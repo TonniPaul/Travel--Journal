2561e0319020abc4320674343dfcc31b848ecdd7
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-16 14:18:23 +0100

    inserted proper link addreses

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,7 +3,7 @@ export const data = [
       id: 1,
       img: "https://www.as-p.com/fileadmin/_processed_/import2018/0/f/csm_image1_20_302c7cb947.jpg?_=1607003855",
       country: "NIGERIA",
-      glink: 'maps.google.com',
+      glink: 'https://maps.app.goo.gl/pGeYrEJpLCM8TGyC8',
       date: "10 May, 2021 - 8 July, 2022",
       title: 'Ikoyi Bridge, Lagos',
       description: "It links the Phase 1 area of Lekki, with Ikoyi district of Lagos. The bridge was commissioned on the 29th of May 2013 by the Governor of Lagos State, Babatunde Raji Fashola."
@@ -13,7 +13,7 @@ export const data = [
       id: 2,
       img: "https://source.unsplash.com/JmuyB_LibRo",
       country : "AUSTRALIA",
-      glink: 'maps.google.com',
+      glink: 'https://maps.app.goo.gl/nYfvKV4ReXCHV1az5',
       date: "27 May, 2021 - 8 Jun, 2021",
       title: 'Sydney Opera House',
       description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
@@ -22,7 +22,7 @@ export const data = [
       id:3,
       img: "https://source.unsplash.com/3PeSjpLVtLg",
       country : "Norway",
-      glink: 'maps.google.com',
+      glink: 'https://maps.app.goo.gl/HdpEjX7YWK2UjvPs8',
       date: "01 Oct, 2021 - 18 Nov, 2021",
       title: 'Geirangerfjord',
       description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
@@ -31,7 +31,7 @@ export const data = [
       id: 4,
       img: "https://source.unsplash.com/WLxQvbMyfas",
       country : "JAPAN",
-      glink: 'maps.google.com',
+      glink: 'https://maps.app.goo.gl/HLwaHyZhc7Gu1h9HA',
       date: "12 Jan, 2021 - 24 Jan, 2021",
       title: 'Mount Fuji',
       description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
